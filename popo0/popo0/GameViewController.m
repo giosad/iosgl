590ca1bb4614bf0666ab9dda0809efaafb0cb21b
Gennadi Iosad <gena@lightricks.com>
2016-02-17 14:21:05 +0200

    fix indentation

--- popo0/popo0/GameViewController.m
+++ popo0/popo0/GameViewController.m
@@ -15,22 +15,22 @@
 // Uniform index.
 enum
 {
-    UNIFORM_MODELVIEWPROJECTION_MATRIX,
-    UNIFORM_NORMAL_MATRIX,
-    NUM_UNIFORMS
+  UNIFORM_MODELVIEWPROJECTION_MATRIX,
+  UNIFORM_NORMAL_MATRIX,
+  NUM_UNIFORMS
 };
 
 const char* uniformNames[NUM_UNIFORMS] = {"modelViewProjectionMatrix",
-                            "normalMatrix"};
+  "normalMatrix"};
 
 GLint uniforms[NUM_UNIFORMS];
 
 // Attribute index.
 enum
 {
-    ATTRIB_VERTEX,
-    ATTRIB_NORMAL,
-    NUM_ATTRIBUTES
+  ATTRIB_VERTEX,
+  ATTRIB_NORMAL,
+  NUM_ATTRIBUTES
 };
 
 const char* uniformNames[NUM_UNIFORMS] = {"modelViewProjectionMatrix",
@@ -38,61 +38,61 @@ const char* uniformNames[NUM_UNIFORMS] = {"modelViewProjectionMatrix",
 
 GLint attribs[NUM_ATTRIBUTES];
 
-GLfloat gCubeVertexData[216] = 
+GLfloat gCubeVertexData[216] =
 {
-    // Data layout for each line below is:
-    // positionX, positionY, positionZ,     normalX, normalY, normalZ,
-    0.5f, -0.5f, -0.5f,        1.0f, 0.0f, 0.0f,
-    0.5f, 0.5f, -0.5f,         1.0f, 0.0f, 0.0f,
-    0.5f, -0.5f, 0.5f,         1.0f, 0.0f, 0.0f,
-    0.5f, -0.5f, 0.5f,         1.0f, 0.0f, 0.0f,
-    0.5f, 0.5f, -0.5f,          1.0f, 0.0f, 0.0f,
-    0.5f, 0.5f, 0.5f,         1.0f, 0.0f, 0.0f,
-    
-    0.5f, 0.5f, -0.5f,         0.0f, 1.0f, 0.0f,
-    -0.5f, 0.5f, -0.5f,        0.0f, 1.0f, 0.0f,
-    0.5f, 0.5f, 0.5f,          0.0f, 1.0f, 0.0f,
-    0.5f, 0.5f, 0.5f,          0.0f, 1.0f, 0.0f,
-    -0.5f, 0.5f, -0.5f,        0.0f, 1.0f, 0.0f,
-    -0.5f, 0.5f, 0.5f,         0.0f, 1.0f, 0.0f,
-    
-    -0.5f, 0.5f, -0.5f,        -1.0f, 0.0f, 0.0f,
-    -0.5f, -0.5f, -0.5f,       -1.0f, 0.0f, 0.0f,
-    -0.5f, 0.5f, 0.5f,         -1.0f, 0.0f, 0.0f,
-    -0.5f, 0.5f, 0.5f,         -1.0f, 0.0f, 0.0f,
-    -0.5f, -0.5f, -0.5f,       -1.0f, 0.0f, 0.0f,
-    -0.5f, -0.5f, 0.5f,        -1.0f, 0.0f, 0.0f,
-    
-    -0.5f, -0.5f, -0.5f,       0.0f, -1.0f, 0.0f,
-    0.5f, -0.5f, -0.5f,        0.0f, -1.0f, 0.0f,
-    -0.5f, -0.5f, 0.5f,        0.0f, -1.0f, 0.0f,
-    -0.5f, -0.5f, 0.5f,        0.0f, -1.0f, 0.0f,
-    0.5f, -0.5f, -0.5f,        0.0f, -1.0f, 0.0f,
-    0.5f, -0.5f, 0.5f,         0.0f, -1.0f, 0.0f,
-    
-    0.5f, 0.5f, 0.5f,          0.0f, 0.0f, 1.0f,
-    -0.5f, 0.5f, 0.5f,         0.0f, 0.0f, 1.0f,
-    0.5f, -0.5f, 0.5f,         0.0f, 0.0f, 1.0f,
-    0.5f, -0.5f, 0.5f,         0.0f, 0.0f, 1.0f,
-    -0.5f, 0.5f, 0.5f,         0.0f, 0.0f, 1.0f,
-    -0.5f, -0.5f, 0.5f,        0.0f, 0.0f, 1.0f,
-    
-    0.5f, -0.5f, -0.5f,        0.0f, 0.0f, -1.0f,
-    -0.5f, -0.5f, -0.5f,       0.0f, 0.0f, -1.0f,
-    0.5f, 0.5f, -0.5f,         0.0f, 0.0f, -1.0f,
-    0.5f, 0.5f, -0.5f,         0.0f, 0.0f, -1.0f,
-    -0.5f, -0.5f, -0.5f,       0.0f, 0.0f, -1.0f,
-    -0.5f, 0.5f, -0.5f,        0.0f, 0.0f, -1.0f
+  // Data layout for each line below is:
+  // positionX, positionY, positionZ,     normalX, normalY, normalZ,
+  0.5f, -0.5f, -0.5f,        1.0f, 0.0f, 0.0f,
+  0.5f, 0.5f, -0.5f,         1.0f, 0.0f, 0.0f,
+  0.5f, -0.5f, 0.5f,         1.0f, 0.0f, 0.0f,
+  0.5f, -0.5f, 0.5f,         1.0f, 0.0f, 0.0f,
+  0.5f, 0.5f, -0.5f,          1.0f, 0.0f, 0.0f,
+  0.5f, 0.5f, 0.5f,         1.0f, 0.0f, 0.0f,
+  
+  0.5f, 0.5f, -0.5f,         0.0f, 1.0f, 0.0f,
+  -0.5f, 0.5f, -0.5f,        0.0f, 1.0f, 0.0f,
+  0.5f, 0.5f, 0.5f,          0.0f, 1.0f, 0.0f,
+  0.5f, 0.5f, 0.5f,          0.0f, 1.0f, 0.0f,
+  -0.5f, 0.5f, -0.5f,        0.0f, 1.0f, 0.0f,
+  -0.5f, 0.5f, 0.5f,         0.0f, 1.0f, 0.0f,
+  
+  -0.5f, 0.5f, -0.5f,        -1.0f, 0.0f, 0.0f,
+  -0.5f, -0.5f, -0.5f,       -1.0f, 0.0f, 0.0f,
+  -0.5f, 0.5f, 0.5f,         -1.0f, 0.0f, 0.0f,
+  -0.5f, 0.5f, 0.5f,         -1.0f, 0.0f, 0.0f,
+  -0.5f, -0.5f, -0.5f,       -1.0f, 0.0f, 0.0f,
+  -0.5f, -0.5f, 0.5f,        -1.0f, 0.0f, 0.0f,
+  
+  -0.5f, -0.5f, -0.5f,       0.0f, -1.0f, 0.0f,
+  0.5f, -0.5f, -0.5f,        0.0f, -1.0f, 0.0f,
+  -0.5f, -0.5f, 0.5f,        0.0f, -1.0f, 0.0f,
+  -0.5f, -0.5f, 0.5f,        0.0f, -1.0f, 0.0f,
+  0.5f, -0.5f, -0.5f,        0.0f, -1.0f, 0.0f,
+  0.5f, -0.5f, 0.5f,         0.0f, -1.0f, 0.0f,
+  
+  0.5f, 0.5f, 0.5f,          0.0f, 0.0f, 1.0f,
+  -0.5f, 0.5f, 0.5f,         0.0f, 0.0f, 1.0f,
+  0.5f, -0.5f, 0.5f,         0.0f, 0.0f, 1.0f,
+  0.5f, -0.5f, 0.5f,         0.0f, 0.0f, 1.0f,
+  -0.5f, 0.5f, 0.5f,         0.0f, 0.0f, 1.0f,
+  -0.5f, -0.5f, 0.5f,        0.0f, 0.0f, 1.0f,
+  
+  0.5f, -0.5f, -0.5f,        0.0f, 0.0f, -1.0f,
+  -0.5f, -0.5f, -0.5f,       0.0f, 0.0f, -1.0f,
+  0.5f, 0.5f, -0.5f,         0.0f, 0.0f, -1.0f,
+  0.5f, 0.5f, -0.5f,         0.0f, 0.0f, -1.0f,
+  -0.5f, -0.5f, -0.5f,       0.0f, 0.0f, -1.0f,
+  -0.5f, 0.5f, -0.5f,        0.0f, 0.0f, -1.0f
 };
 
 @interface GameViewController () {
-
-    
-    GLKMatrix4 _modelViewProjectionMatrix;
-    GLKMatrix3 _normalMatrix;
-    float _rotation;
-    GLuint _vertexArray;
-    GLuint _vertexBuffer;
+  
+  
+  GLKMatrix4 _modelViewProjectionMatrix;
+  GLKMatrix3 _normalMatrix;
+  float _rotation;
+  GLuint _vertexArray;
+  GLuint _vertexBuffer;
 }
 @property (strong, nonatomic) GLShaderMgr *shaderHelper;
 @property (strong, nonatomic) EAGLContext *context;
@@ -105,125 +105,125 @@ GLfloat gCubeVertexData[216] =
 
 - (void)viewDidLoad
 {
-    [super viewDidLoad];
-    
-    self.context = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2];
-
-    if (!self.context) {
-        NSLog(@"Failed to create ES context");
-    }
-    
-    GLKView *view = (GLKView *)self.view;
-    view.context = self.context;
-    view.drawableDepthFormat = GLKViewDrawableDepthFormat24;
-    
-    [self setupGL];
+  [super viewDidLoad];
+  
+  self.context = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2];
+  
+  if (!self.context) {
+    NSLog(@"Failed to create ES context");
+  }
+  
+  GLKView *view = (GLKView *)self.view;
+  view.context = self.context;
+  view.drawableDepthFormat = GLKViewDrawableDepthFormat24;
+  
+  [self setupGL];
 }
 
 - (void)dealloc
-{    
-    [self tearDownGL];
-    
-    if ([EAGLContext currentContext] == self.context) {
-        [EAGLContext setCurrentContext:nil];
-    }
+{
+  [self tearDownGL];
+  
+  if ([EAGLContext currentContext] == self.context) {
+    [EAGLContext setCurrentContext:nil];
+  }
 }
 
 - (void)didReceiveMemoryWarning
 {
-    [super didReceiveMemoryWarning];
-
-    if ([self isViewLoaded] && ([[self view] window] == nil)) {
-        self.view = nil;
-        
-        [self tearDownGL];
-        
-        if ([EAGLContext currentContext] == self.context) {
-            [EAGLContext setCurrentContext:nil];
-        }
-        self.context = nil;
+  [super didReceiveMemoryWarning];
+  
+  if ([self isViewLoaded] && ([[self view] window] == nil)) {
+    self.view = nil;
+    
+    [self tearDownGL];
+    
+    if ([EAGLContext currentContext] == self.context) {
+      [EAGLContext setCurrentContext:nil];
     }
-
-    // Dispose of any resources that can be recreated.
+    self.context = nil;
+  }
+  
+  // Dispose of any resources that can be recreated.
 }
 
 - (BOOL)prefersStatusBarHidden {
-    return YES;
+  return YES;
 }
 
 - (void)setupGL
 {
-    [EAGLContext setCurrentContext:self.context];
-    
-    [self.shaderHelper loadShadersByName:@"Shader"];
-    
-    glEnable(GL_DEPTH_TEST);
-    
-    glGenVertexArraysOES(1, &_vertexArray);
-    glBindVertexArrayOES(_vertexArray);
-    
-    glGenBuffers(1, &_vertexBuffer);
-    glBindBuffer(GL_ARRAY_BUFFER, _vertexBuffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(gCubeVertexData), gCubeVertexData, GL_STATIC_DRAW);
-    
-    glEnableVertexAttribArray(attribs[ATTRIB_VERTEX]);
-    glVertexAttribPointer(attribs[ATTRIB_VERTEX], 3, GL_FLOAT, GL_FALSE, 24, BUFFER_OFFSET(0));
-    glEnableVertexAttribArray(attribs[ATTRIB_NORMAL]);
-    glVertexAttribPointer(attribs[ATTRIB_NORMAL], 3, GL_FLOAT, GL_FALSE, 24, BUFFER_OFFSET(12));
-    
-    glBindVertexArrayOES(0);
+  [EAGLContext setCurrentContext:self.context];
+  
+  [self.shaderHelper loadShadersByName:@"Shader"];
+  
+  glEnable(GL_DEPTH_TEST);
+  
+  glGenVertexArraysOES(1, &_vertexArray);
+  glBindVertexArrayOES(_vertexArray);
+  
+  glGenBuffers(1, &_vertexBuffer);
+  glBindBuffer(GL_ARRAY_BUFFER, _vertexBuffer);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(gCubeVertexData), gCubeVertexData, GL_STATIC_DRAW);
+  
+  glEnableVertexAttribArray(attribs[ATTRIB_VERTEX]);
+  glVertexAttribPointer(attribs[ATTRIB_VERTEX], 3, GL_FLOAT, GL_FALSE, 24, BUFFER_OFFSET(0));
+  glEnableVertexAttribArray(attribs[ATTRIB_NORMAL]);
+  glVertexAttribPointer(attribs[ATTRIB_NORMAL], 3, GL_FLOAT, GL_FALSE, 24, BUFFER_OFFSET(12));
+  
+  glBindVertexArrayOES(0);
 }
 
 - (void)tearDownGL
 {
-    [EAGLContext setCurrentContext:self.context];
-    
-    glDeleteBuffers(1, &_vertexBuffer);
-    glDeleteVertexArraysOES(1, &_vertexArray);
+  [EAGLContext setCurrentContext:self.context];
   
-    [self.shaderHelper deleteProgram];
-
+  glDeleteBuffers(1, &_vertexBuffer);
+  glDeleteVertexArraysOES(1, &_vertexArray);
+  
+  [self.shaderHelper deleteProgram];
+  
 }
 
 #pragma mark - GLKView and GLKViewController delegate methods
 
 - (void)update
 {
-    float aspect = fabs(self.view.bounds.size.width / self.view.bounds.size.height);
-    GLKMatrix4 projectionMatrix = GLKMatrix4MakePerspective(GLKMathDegreesToRadians(65.0f), aspect, 0.1f, 100.0f);
+  float aspect = fabs(self.view.bounds.size.width / self.view.bounds.size.height);
+  GLKMatrix4 projectionMatrix = GLKMatrix4MakePerspective(GLKMathDegreesToRadians(65.0f), aspect, 0.1f, 100.0f);
   
-    GLKMatrix4 baseModelViewMatrix = GLKMatrix4MakeTranslation(0.0f, 0.0f, -4.0f);
-    baseModelViewMatrix = GLKMatrix4Rotate(baseModelViewMatrix, _rotation, 0.0f, 1.0f, 0.0f);
-
-    // Compute the model view matrix for the object rendered with ES2
-    GLKMatrix4 modelViewMatrix = GLKMatrix4MakeTranslation(0.0f, 0.0f, -1.5f);
-    modelViewMatrix = GLKMatrix4MakeTranslation(0.0f, 0.0f, 1.5f);
-    modelViewMatrix = GLKMatrix4Rotate(modelViewMatrix, _rotation, 1.0f, 1.0f, 1.0f);
-    modelViewMatrix = GLKMatrix4Multiply(baseModelViewMatrix, modelViewMatrix);
-    
-    _normalMatrix = GLKMatrix3InvertAndTranspose(GLKMatrix4GetMatrix3(modelViewMatrix), NULL);
-    
-    _modelViewProjectionMatrix = GLKMatrix4Multiply(projectionMatrix, modelViewMatrix);
-    
-    _rotation += self.timeSinceLastUpdate * 0.5f;
+  GLKMatrix4 baseModelViewMatrix = GLKMatrix4MakeTranslation(0.0f, 0.0f, -4.0f);
+  baseModelViewMatrix = GLKMatrix4Rotate(baseModelViewMatrix, _rotation, 0.0f, 1.0f, 0.0f);
+  
+  // Compute the model view matrix for the object rendered with ES2
+  GLKMatrix4 modelViewMatrix = GLKMatrix4MakeTranslation(0.0f, 0.0f, -1.5f);
+  modelViewMatrix = GLKMatrix4MakeTranslation(0.0f, 0.0f, 1.5f);
+  modelViewMatrix = GLKMatrix4Rotate(modelViewMatrix, _rotation, 1.0f, 1.0f, 1.0f);
+  modelViewMatrix = GLKMatrix4Multiply(baseModelViewMatrix, modelViewMatrix);
+  
+  _normalMatrix = GLKMatrix3InvertAndTranspose(GLKMatrix4GetMatrix3(modelViewMatrix), NULL);
+  
+  _modelViewProjectionMatrix = GLKMatrix4Multiply(projectionMatrix, modelViewMatrix);
+  
+  _rotation += self.timeSinceLastUpdate * 0.5f;
 }
 
 - (void)glkView:(GLKView *)view drawInRect:(CGRect)rect
 {
-    glClearColor(0.65f, 0.65f, 0.65f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    
-    glBindVertexArrayOES(_vertexArray);
-    
-    glDrawArrays(GL_TRIANGLES, 0, 36);
-    
-    // Render the object again with ES2
-    glUseProgram(self.shaderHelper.program);
-    
-    glUniformMatrix4fv(uniforms[UNIFORM_MODELVIEWPROJECTION_MATRIX], 1, 0, _modelViewProjectionMatrix.m);
-    glUniformMatrix3fv(uniforms[UNIFORM_NORMAL_MATRIX], 1, 0, _normalMatrix.m);
-    
-    glDrawArrays(GL_TRIANGLES, 0, 36);
+  glClearColor(0.65f, 0.65f, 0.65f, 1.0f);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  
+  glBindVertexArrayOES(_vertexArray);
+  
+  glDrawArrays(GL_TRIANGLES, 0, 36);
+  
+  // Render the object again with ES2
+  glUseProgram(self.shaderHelper.program);
+  
+  glUniformMatrix4fv(uniforms[UNIFORM_MODELVIEWPROJECTION_MATRIX], 1, 0, _modelViewProjectionMatrix.m);
+  glUniformMatrix3fv(uniforms[UNIFORM_NORMAL_MATRIX], 1, 0, _normalMatrix.m);
+  
+  glDrawArrays(GL_TRIANGLES, 0, 36);
 }
 
 @end
